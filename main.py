e9472c4a6f022570a77145466e8bf0a238a93060
shanti-gzmn <shantiguzmag@gmail.com>
2023-08-25 13:18:40 -0600

    Templates in html added

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+todos = ['Comprar cafe', 'Enviar solicitud', 'Entregar video a productor']
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+    return render_template('hello.html', **context)
