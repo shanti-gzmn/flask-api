82b0511ce05f9acfbd3627187c2159979568ffce
shanti-gzmn <shantiguzmag@gmail.com>
2023-09-12 14:50:03 -0600

    Auth added

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
+from flask_login import login_required, current_user
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 # instances
 app = create_app()
-
-todos = ['Comprar cafe', 'Enviar solicitud', 'Entregar video a productor']
 
 @app.errorhandler(404)
 def not_found(error):
@@ -24,14 +23,15 @@ def index():
     return response
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
-    username = session.get('username')
+    username = current_user.id
+
     context = {
         'user_ip': user_ip,
-        'todos': todos,
-        'username': username,
-        'login_form': login_form
-    }    
+        'todos': get_todos(user_id=username),
+        'username': username
+    }
+
     return render_template('hello.html', **context)
